446c42d3f352f7eb22783b91fd087f3ea51e8014
Preetkanwal-kaur09 <preetkhurana2001@gmail.com>
2026-07-16 23:13:39 -0700

    Add basic arithmetic calculator in C++
    Implement a basic arithmetic calculator that performs addition, subtraction, multiplication, division, and modulus operations on two numbers.

---
+++ 01-Basic-Programs/Arithmetic_Calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sum, sub, multi, divide, modu;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    sum = a + b;
+    sub = a - b;
+    multi = a * b;
+    divide = a / b;
+    modu = a % b;
+    cout<<"Addition: "<<sum<<endl;
+    cout<<"Substraction: "<<sub<<endl;
+    cout<<"Multiplication: "<<multi<<endl;
+    cout<<"Division: "<<divide<<endl;
+    cout<<"Modulus: "<<modu;
+    
+    
+    
+    
+
+    return 0;
+}
