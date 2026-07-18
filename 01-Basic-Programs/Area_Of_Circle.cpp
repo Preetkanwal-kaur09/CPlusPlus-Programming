7976144f89fb474cd59f12516ad146275a9ecbb1
Preetkanwal-kaur09 <preetkhurana2001@gmail.com>
2026-07-17 23:13:43 -0700

    Add Area of Circle program in C++

---
+++ 01-Basic-Programs/Area_Of_Circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+    double r;
+    cout<<"Enter radius: ";
+    cin>>r;
+    double area = 3.14 *r * r;
+    cout<<"Area is: "<<area;
+    
+
+    return 0;
+}
