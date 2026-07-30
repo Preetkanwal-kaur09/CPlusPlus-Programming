f5a9d9745d017957f91a487d55d79c2d7bb7ec09
Preetkanwal-kaur09 <preetkhurana2001@gmail.com>
2026-07-30 00:11:02 -0700

    Add Table of N program in C++

---
+++ 03-Loops/Table_Of_N.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a Number: ";
+    cin>>n;
+    for(int i=n;i<=n*10;i+=n)
+    {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
