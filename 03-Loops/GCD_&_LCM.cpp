41c2ff506c879d8ab14fd00ada17de1209374af1
Preetkanwal-kaur09 <preetkhurana2001@gmail.com>
2026-07-30 00:43:14 -0700

    Fix GCD calculation loop structure

--- 03-Loops/GCD_&_LCM.cpp
+++ 03-Loops/GCD_&_LCM.cpp
@@ -2,22 +2,24 @@
 using namespace std;
 int main()
 {
-    int a,b;
+    int a, b;
     cout<<"Enter Two Numbers: ";
     cin>>a>>b;
     
-    int GCD = 1;
+    int gcd =1;
     
     int small;
     if(a < b) small = a;
     else small = b;
     
     for(int i=1; i<=small; i++){
-        if(a%i == 0 && b%i == 0){
-              GCD = i;
+        if(a%i == 0 && b%i == 0){   
+            gcd = i;
         }
     }
-    cout<<"GCD is: "<<GCD;
+    int lcm = (a * b) / gcd;
+    cout<<"GCD is: "<<gcd<<endl;
+    cout<<"LCM is: "<<lcm;
 
     return 0;
 }
